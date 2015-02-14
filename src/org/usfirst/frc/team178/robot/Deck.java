d4b52cc6042ff4ae2aef2cc95ed0da70dc7faff0
Rohan Parikh <rohan.farmington@gmail.com>
2015-02-14 13:38:49 -0500

    fixed EVERYTHING. Safe supposed to be Not Safe, pulled d and made it direction, now a field not an argument for the boolean, made it output the values (need to take those out once we're 100% it's right), direction was flipped

--- src/org/usfirst/frc/team178/robot/Deck.java
+++ src/org/usfirst/frc/team178/robot/Deck.java
@@ -8,41 +8,53 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Deck implements RunningComponent {
 	
 	private Joystick joystick;
-	private DigitalInput outerLimit;
-	private DigitalInput innerLimit;
+	private DigitalInput frontLimit;
+	private DigitalInput backLimit;
 	private Talon motor;
+	private double direction;
 	
-	public Deck(Joystick joystick, DigitalInput outerLimit,
-			DigitalInput innerLimit, Talon motor) {
+	public Deck(Joystick joystick, DigitalInput backLimit,
+			DigitalInput frontLimit, Talon motor) {
 		super();
 		this.joystick = joystick;
-		this.outerLimit = outerLimit;
-		this.innerLimit = innerLimit;
+		this.frontLimit = frontLimit;
+		this.backLimit = backLimit;
 		this.motor = motor;
+		this.direction = 0;
 	}
 
 	@Override
 	public void teleop() {
-		if(joystick.getRawButton(3))
-			moveMotor(-1.0);
-		else if(joystick.getRawButton(4))
-			moveMotor(1.0);
+		this.motor.set(direction);
+		setDirection();
+		//this.motor.set(joystick.getY()*.25);
+		
+		SmartDashboard.putBoolean("Front Limit", this.frontLimit.get());
+		SmartDashboard.putBoolean("Back Limit", this.backLimit.get());
 	}
 
-	private boolean moveMotor(double d) {
-		boolean isSafe = 
-				(d>0 && !this.outerLimit.get()) || //if we're moving out but not too far OR
-				(d<0 && !this.innerLimit.get()) || //if we're moving in but not too close OR
-				(d<0.001 && d >-0.001); //if the motor shouldn't move...
+	private boolean setDirection() {
 		
-		if(!isSafe){
-			d = 0; //we're not safe; prevent the motor from moving
+		if(joystick.getRawButton(3))
+			direction= -.5;
+		else if(joystick.getRawButton(4))
+			direction = .5;
+		else if(joystick.getRawButton (5))
+			direction=0;
+		
+		boolean isNotSafe = 
+				(direction<0 && !this.frontLimit.get()) || //if we're moving out but not too far OR
+				(direction>0 && !this.backLimit.get()); //if we're moving in but not too close OR
+		
+		if(isNotSafe){
+			direction = 0; //we're not safe; prevent the motor from moving
 		}
 		
-		motor.set(d); //move the motor (or don't if unsafe)
-
-		SmartDashboard.putBoolean("Motor is safe?", isSafe); //notify users of safety
-		return isSafe;
+		//move the motor (or don't if unsafe)
+		SmartDashboard.putBoolean("Motor is not safe?", isNotSafe); //notify users of safety
+		SmartDashboard.putNumber("Direction",direction);// for testing purposes
+		
+		return isNotSafe;
 		
 	}
 
