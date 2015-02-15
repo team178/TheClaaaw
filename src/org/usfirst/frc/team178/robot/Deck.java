cf34a90abb05278bda3e38b04dfab0c77d9b66c2
Rohan Parikh <rohan.farmington@gmail.com>
2015-02-15 13:19:42 -0500

    Revert merge with Claw, take direction as param to setDirection

--- src/org/usfirst/frc/team178/robot/Deck.java
+++ src/org/usfirst/frc/team178/robot/Deck.java
@@ -11,7 +11,6 @@ public class Deck implements RunningComponent {
 	private DigitalInput frontLimit;
 	private DigitalInput backLimit;
 	private Talon motor;
-	private double direction;
 	
 	public Deck(Joystick joystick, DigitalInput backLimit,
 			DigitalInput frontLimit, Talon motor) {
@@ -20,27 +19,26 @@ public class Deck implements RunningComponent {
 		this.frontLimit = frontLimit;
 		this.backLimit = backLimit;
 		this.motor = motor;
-		this.direction = 0;
 	}
 
 	@Override
 	public void teleop() {
-		this.motor.set(direction);
-		setDirection();
+		double direction;
+		if(joystick.getRawButton(3))
+			direction= -1;
+		else if(joystick.getRawButton(4))
+			direction = 1;
+		else
+			direction=0;
+		
+		setDirection(direction);
 		//this.motor.set(joystick.getY()*.25);
 		
 		SmartDashboard.putBoolean("Front Limit", this.frontLimit.get());
 		SmartDashboard.putBoolean("Back Limit", this.backLimit.get());
 	}
 
-	private boolean setDirection() {
-		
-		if(joystick.getRawButton(3))
-			direction= -.5;
-		else if(joystick.getRawButton(4))
-			direction = .5;
-		else if(joystick.getRawButton (5))
-			direction=0;
+	private boolean setDirection(double direction) {
 		
 		boolean isNotSafe = 
 				(direction<0 && !this.frontLimit.get()) || //if we're moving out but not too far OR
@@ -53,6 +51,8 @@ public class Deck implements RunningComponent {
 		//move the motor (or don't if unsafe)
 		SmartDashboard.putBoolean("Motor is not safe?", isNotSafe); //notify users of safety
 		SmartDashboard.putNumber("Direction",direction);// for testing purposes
+		
+		this.motor.set(direction);
 		
 		return isNotSafe;
 		
