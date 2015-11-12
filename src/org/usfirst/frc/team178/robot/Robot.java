3620f3cf118ef961af05644a945db262c148500b
Giselle Koo <gisellegk@gmail.com>
2015-11-12 17:37:58 -0500

    Added really important thing to comment.

--- src/org/usfirst/frc/team178/robot/Robot.java
+++ src/org/usfirst/frc/team178/robot/Robot.java
@@ -57,7 +57,7 @@ public class Robot extends IterativeRobot {
 
 	/**
 	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * used for any initialization code. TEST EDIT YAY
 	 */
 	
 	private RunningComponent[] components = { 
